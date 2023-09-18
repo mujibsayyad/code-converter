753bcfb8050ca5ce5410c6c26b57e4bd7d67e320
Mujib Sayyad <63368252+mujibsayyad@users.noreply.github.com>
2023-09-18 14:10:26 +0530

    Update serverlessAPI.ts

--- src/pages/api/serverlessAPI.ts
+++ src/pages/api/serverlessAPI.ts
@@ -26,7 +26,7 @@ export default async function handler(
         model: 'text-davinci-003',
         prompt: `Translate this code from ${language} into ${convertToLanguage} \n${code}\n`,
         temperature: 0,
-        max_tokens: 150,
+        max_tokens: 500,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
